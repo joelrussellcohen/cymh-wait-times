c2ea0ae391bca8cb77927445e4153596dcb3ccad
Joel Cohen <joelcohen@Joels-MacBook-Pro.local>
2020-10-07 18:39:29 -0700

    Turned combined bar graph into a function combined_bar(x,y) which plots columns in y against column x. See comments in plot.py for more info.

--- plot.py
+++ plot.py
@@ -2,15 +2,57 @@ import matplotlib.pyplot as pyplot
 import numpy as np
 import pandas as pd
 
+# Function scatter_plot
 # Plot the data as a scatter plot
-def scatter_plot(x, y, filename=None, xlabel=None, ylabel=None, show=True):
+# Inputs:
+# x - an array-like object that will be plotted with y along the horizontal axis
+# y - an array-like object that will be plotted with x along the vertical axis
+# filename - if specified, a string which contains the path to save the plot image to
+# xlabel - if specified, a string which denotes the x label of the plot
+# ylabel - if specified, a string which denotes the y label og the plot
+# title - if specified, a string which denotes the title of the plot
+# show - default: True, if show=False, suppresses display of the function
+# Returns: none
+def scatter_plot(x, y, filename=None, xlabel=None, ylabel=None, title=None, show=True):
     pyplot.plot(x, y, 'ro')
     pyplot.xlabel(xlabel)
     pyplot.ylabel(ylabel)
     if filename:
-        pyplot.savefig('./saved_images/%s' % filename, bbox_inches='tight', pad_inches=0.2)
+        pyplot.savefig(filename, bbox_inches='tight', pad_inches=0.2)
     if show:
         pyplot.show()
+
+# Function combined_bar
+# Plot the data as a scatter plot
+# x - an m dimentional array-like object which sets the horizontal positions of the bar graphs
+# y - an m, n dimentional array-like object which is represents a list of data to plot against x
+# filename - if specified, a string which contains the path to save the plot image to
+# xlabel - if specified, a string which denotes the contents of the x-axis
+# ylabel - if specified, a string list of strings which denote the labels for the bars
+# title - if specified, a string which denotes the title of the plot
+# show - default: True, if show=False, suppresses display of the function
+# Returns: None
+def combined_bar(x, y_list, filename=None, xlabel=None, ylabel=None, title=None, width=0.25, show=True):
+    for i in range(len(y_list[0,:])):
+        pyplot.bar([s + i*width for s in x], y_list[:,i], width=width)
+    pyplot.xlabel(xlabel)
+
+    if(ylabel and type(ylabel) is not str):
+        ylabel_str = ''
+        for label in ylabel:
+            ylabel_str += '%s\n' % label
+        ylabel_str = ylabel_str[:-1]
+        pyplot.ylabel(ylabel_str)
+    else:
+        pyplot.ylabel(ylabel)
+
+    pyplot.title(title)
+
+    if filename:
+        pyplot.savefig(filename, bbox_inches='tight', pad_inches=0.2)
+    if show:
+        pyplot.show()
+
 
 if __name__ == "__main__":
     # Extract the raw data from the text file
@@ -39,17 +81,17 @@ if __name__ == "__main__":
     sdas = data[:,0] # Service delivery areas
     scale_factor = 10 # Factor by which we are scaling number of new clients
     x_scaled = x / scale_factor
+    bars = np.column_stack((x_scaled, y))
+    ylabels = ['Number of new clients / 10', 'Average wait time in days for new clients']
+    xlabel = 'Service Delivery areas'
 
-    pyplot.bar(sdas, x_scaled, width=0.25)
-    pyplot.bar([s + 0.3 for s in sdas], y, width=0.25)
-
-    pyplot.ylabel('Number of new clients / 10\nAverage wait time in days for new clients')
-    pyplot.xlabel('Service Delivery Area #')
+    combined_bar(sdas, bars, xlabel=xlabel, ylabel=ylabels, show=False)
     pyplot.xticks(sdas)
 
     # Uncomment to the next comment to save the plot
     #pyplot.savefig('./saved_images/combined_bar.png', bbox_inches='tight', pad_inches=0.2)
     pyplot.show()
+
 
     # Plot the ratio of average wait time per number of new clients to each service delivery area as a bar graph
     ytox = [y[i]/x[i] for i in range(0,x.size)]
